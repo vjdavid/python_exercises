1ed3050abc6b65f4f6ea99097adcb78ed7ba28b5
David Jarillo <vjdavid621@gmail.com>
2018-03-11 23:30:39 -0700

    fixed issues of formatting in list program

--- list.py
+++ list.py
@@ -5,5 +5,5 @@ list = [2, 'Mangos', 3, 0.49, 5, 0.0000123, 'manzanas', 113.52342324, 0.54,
 
 size = 0
 while size < 10:
-    print("{} es: {}".format(list[size], type(list[size])))
+    print("El tipo de dato del elemento [{}] {} es: {}".format(size, list[size], type(list[size])))
     size += 1
