02e35d36def2bb445118d22dd49e2b669be10b4e
David Jarillo <vjdavid621@gmail.com>
2018-03-11 23:56:49 -0700

    first program poem finished

---
+++ first_unit/poem.py
@@ -0,0 +1,5 @@
+#Victor David Jarillo Davila
+
+poem = "Nunca vi una criatura salvaje\nsentir lastima por si misma\nUn ave pequeña caera de una rama muerta de frio,\nsin jamas haber sentido lastima por si misma."
+
+print(poem)
