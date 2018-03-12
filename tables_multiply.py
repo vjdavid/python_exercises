ef4472c35906412d16df52799e999dafce1cdb64
David Jarillo <vjdavid621@gmail.com>
2018-03-11 23:21:13 -0700

    finished exercise covering for nested loops

---
+++ tables_multiply.py
@@ -0,0 +1,7 @@
+#Victor David Jarillo Davila
+
+for multiply in range(1,11):
+    print("Tabla de multiplicar del {}".format(multiply))
+    base = multiply
+    for multiplying in range(1,11):
+        print("{} X {} = {}".format(multiplying, base, multiplying * base))
