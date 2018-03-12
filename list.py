3b925557933b729f626b441ac24abdeec58ab40e
David Jarillo <vjdavid621@gmail.com>
2018-03-11 18:19:19 -0700

    finished first two programs

---
+++ list.py
@@ -0,0 +1,9 @@
+#Victor David Jarillo Davila
+
+list = [2, 'Mangos', 3, 0.49, 5, 0.0000123, 'manzanas', 113.52342324, 0.54,
+        'melones']
+
+size = 0
+while size < 10:
+    print("{} es: {}".format(list[size], type(list[size])))
+    size += 1
---
+++ numbers.py
@@ -0,0 +1,6 @@
+#Victor David Jarillo Davila
+
+for number in range(1, 3):
+    for holis in range(0, number):
+        print("Interno: {}".format(number))
+    print("\tExterno: {}".format(number))
