3d8985302f77bf7e8ba8b88e8e3ec1f0dc6f5f2c
David Jarillo <vjdavid621@gmail.com>
2018-03-13 20:13:37 -0700

    added functionality in 4math function

--- first_unit/4math_functions.py
+++ first_unit/4math_functions.py
@@ -18,8 +18,22 @@ print("tan de {} y {}".format(given_numbers[0], given_numbers[1]))
 print("Valores originales: A = {}, B = {}".format(given_numbers[0],
     given_numbers[1]))
 
-square_root_one = math.sqrt(given_numbers[0])
-square_toot_two = math.sqrt(given_numbers[1])
-print("La raiz cuadrada de {} es {} y de {} es {}".format(given_numbers[0],
-    square_root_one, given_numbers[1], square_toot_two))
+c = math.sqrt(given_numbers[0])
+d = math.sqrt(given_numbers[1])
+print("La raiz cuadrada de {} es {} y de {} es {}".format(given_numbers[0], c, given_numbers[1], d))
 
+e = math.pow(given_numbers[0], 2)
+f = math.pow(given_numbers[1], 2)
+print("La potencia de {} es {} y de {} es {}".format(given_numbers[0], e, given_numbers[1], f))
+
+g = math.cos(given_numbers[0])
+h = math.cos(given_numbers[1])
+print("El coseno de {} es {} y de {} es {}".format(given_numbers[0], g, given_numbers[1], h))
+
+h = math.sin(given_numbers[0])
+i = math.sin(given_numbers[1])
+print("El seno de {} es {} y de {} es {}".format(given_numbers[0], h, given_numbers[1], i))
+
+j = math.tan(given_numbers[0])
+k = math.tan(given_numbers[1])
+print("La tangente de {} es {} y de {} es {}".format(given_numbers[0], j, given_numbers[1], k))
